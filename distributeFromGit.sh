71de4fcba66c460c988f035f471757adf25d4733
Hasan <martinhbramwell@gmail.com>
2011-12-13 14:08:49 -0500

    Use warehouseman.com instead of justtrade.net Point resolv.conf to self

--- distributeFromGit.sh
+++ distributeFromGit.sh
@@ -4,6 +4,7 @@ cp ./etc/network/interfaces		/etc/network/interfaces
 cp ./etc/resolv.conf			/etc/resolv.conf
 cp ./etc/hosts				/etc/hosts
 
+#   sudo /etc/init.d/bin9 restart
 #   sudo /etc/init.d/networking restart
 
 
