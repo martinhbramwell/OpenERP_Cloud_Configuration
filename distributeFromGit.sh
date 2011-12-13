44880fc68e3c33a54b958d93e596fffce38d7ad4
Hasan <martinhbramwell@gmail.com>
2011-12-13 10:50:32 -0500

    Make sure the same name is used throughout.

--- distributeFromGit.sh
+++ distributeFromGit.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 # script to replace my cloud DNS configuration from files retrieved from GitHub.
-cp ./etc/network/interfaces /etc/network/interfaces
-cp ./etc/resolv.conf        /etc/resolv.conf
-cp ./etc/hosts              /etc/hosts
+cp ./etc/network/interfaces		/etc/network/interfaces
+cp ./etc/resolv.conf			/etc/resolv.conf
+cp ./etc/hosts				/etc/hosts
 
 #   sudo /etc/init.d/networking restart
 
+
+mkdir -p /etc/bind/zones
+
+cp ./etc/bind/named.conf.options	/etc/bind/
+cp ./etc/bind/named.conf.local		/etc/bind/
+
+cp ./etc/bind/zones/*			/etc/bind/zones
 
