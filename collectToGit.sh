1d8f4d5e98a0b129991d84a8a2a65fb7dc6cd0f6
Hasan <martinhbramwell@gmail.com>
2011-12-13 09:55:32 -0500

    Initial naming files.

--- collectToGit.sh
+++ collectToGit.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 # script to collect all my cloud DNS configuration into this directory.
+mkdir -p ./etc/network
+mkdir -p ./etc/bind/zones
 cp -u /etc/network/interfaces ./etc/network/interfaces
 cp -u /etc/resolv.conf ./etc/resolv.conf
 cp -u /etc/hosts ./etc/hosts
+
+cp -u /etc/bind/named.conf.options ./etc/bind/
+cp -u /etc/bind/named.conf.local ./etc/bind/
+
+cp -u /etc/bind/zones/* ./etc/bind/zones
 
 
