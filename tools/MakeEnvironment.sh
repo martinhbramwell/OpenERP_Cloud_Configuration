c55f88011cae6cdfdc62deb41ede2951312037b2
Hasan <martinhbramwell@gmail.com>
2011-12-29 07:24:52 -0500

    An environment file generator.

---
+++ tools/MakeEnvironment.sh
@@ -0,0 +1,19 @@
+echo "# Generated file -- run MakeEnvironment.sh to recreate it." > environment
+echo "JAVA_HOME=$JAVA_HOME" >> environment
+echo "M2_HOME=$M2_HOME" >> environment
+echo "MAVEN_HOME=$MAVEN_HOME" >> environment
+echo "M2_REPO=$M2_REPO" >> environment
+echo "M2=$M2" >> environment
+echo "CATALINA_HOME=$CATALINA_HOME" >> environment
+echo "TOMCAT_USER=$TOMCAT_USER" >> environment
+echo "SRV_CONFIG=$SRV_CONFIG" >> environment
+echo "# Path build from some pieces above and existing path." >> environment
+echo "PATH=$PATH" >> environment
+echo "#" >> environment
+
+
+
+
+
+
+
