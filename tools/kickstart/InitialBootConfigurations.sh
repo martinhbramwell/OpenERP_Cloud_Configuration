c9b644bb1f6b06893f206309a2249f2b0cb72dc1
Hasan <martinhbramwell@gmail.com>
2012-01-03 20:53:11 -0500

    When running as root can't refer to ~

--- tools/kickstart/InitialBootConfigurations.sh
+++ tools/kickstart/InitialBootConfigurations.sh
@@ -6,18 +6,15 @@
 case "$1" in
 start)
 #
-	mkdir -p /home/yourself/tmp
-	cd /home/yourself/tmp
+   mkdir -p /home/yourself/tmp
+   cd /home/yourself/tmp
 
    whoami > /home/yourself/tmp/pwd.txt
    pwd >> /home/yourself/tmp/pwd.txt
-   cd ~/
-   pwd >> /home/yourself/tmp/pwd.txt
-   cat /home/yourself/tmp/pwd.txt
 
-#	rm -f ./First*
-#	wget https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master/tools/kickstart/FirstBootConfigurations.sh
-#	chmod +x ./FirstBootConfigurations.sh
+   rm -f ./First*
+   wget https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master/tools/kickstart/FirstBootConfigurations.sh
+   chmod +x ./FirstBootConfigurations.sh
 
 ;;
 
