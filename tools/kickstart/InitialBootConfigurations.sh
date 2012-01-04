92785412d599cef410a2b6f1f355bee925c052ff
Hasan <martinhbramwell@gmail.com>
2012-01-04 10:24:18 -0500

    This file now calls in multiple files and executes them in sequence.

--- tools/kickstart/InitialBootConfigurations.sh
+++ tools/kickstart/InitialBootConfigurations.sh
@@ -8,11 +8,21 @@ start)
 #
    mkdir -p /home/yourself/
    cd /home/yourself
+
    rm -f ./First*
+
    wget https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master/tools/kickstart/FirstBootConfigurations.sh
    chmod +x ./FirstBootConfigurations.sh
    ./FirstBootConfigurations.sh
-
+#
+   wget https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master/tools/kickstart/FirstBootJenkinsConfigurations.sh
+   chmod +x ./FirstBootJenkinsConfigurations.sh
+   ./FirstBootJenkinsConfigurations.sh
+#
+   wget https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master/tools/kickstart/FirstBootCleanUp.sh
+   chmod +x ./FirstBootCleanUp.sh
+   ./FirstBootCleanUp.sh
+#
 
 ;;
 
