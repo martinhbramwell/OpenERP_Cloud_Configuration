3733565839b976581c036b07107fe681c3e5226d
Hasan <martinhbramwell@gmail.com>
2012-01-06 16:51:59 -0500

    Better to use localhost

--- tools/JenkinsRequiredVars.sh
+++ tools/JenkinsRequiredVars.sh
@@ -1,4 +1,5 @@
-export JENKINS_URL=http://continuous.warehouseman.com/jenkins/
+#export JENKINS_URL=http://continuous.warehouseman.com/jenkins/
+export JENKINS_URL=http://localhost/jenkins/
 export JENKINS_VERSION=1.4
 
 
