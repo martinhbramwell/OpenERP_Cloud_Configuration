67835395cc140d4f71c373984d30e5191aca8033
Hasan <martinhbramwell@gmail.com>
2012-01-06 05:47:09 -0500

    Eliminate bad practice of using "/" in an rm command.

--- tools/kickstart/FirstBootCleanUp.sh
+++ tools/kickstart/FirstBootCleanUp.sh
@@ -1,8 +1,12 @@
 echo "Clean up."
 cd $ADMIN_USERZ_HOME/
+
+echo "Work dir"
 rm -fr $ADMIN_USERZ_WORK_DIR/
 
-rm -f /etc/rc2.d/S99xibc
+echo "Deactivate rc2 script"
+cd /etc/rc2.d/
+rm -f S99xibc
 
 echo "Can we see it? $CAN_WE_SEE_THIS_INSIDE_THE_SCRIPTS"
 
