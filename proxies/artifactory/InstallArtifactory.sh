951b05bf968bfb2d8b42f7d3b76977f69a7bb2a4
Hasan <martinhbramwell@gmail.com>
2012-01-08 06:54:56 -0500

    Added download commands.

--- proxies/artifactory/InstallArtifactory.sh
+++ proxies/artifactory/InstallArtifactory.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 # This script installs artifactory in TomCat.
- 
+mkdir -p ~/Downloads
+cd ~/Downloads
+rm -fr artifactory-2.4.2
+#
+wget -N http://downloads.sourceforge.net/project/artifactory/artifactory/2.4.2/artifactory-2.4.2.zip
+unzip artifactory-2.4.2 artifactory-2.4.2/webapps/artifactory.war
+sudo mv artifactory-2.4.2/* /usr/share/tomcat
+rm -fr ./artifactory-2.4.2
+#
+#
+
+
