9d53ad5b80edb65b77d879e07fc9e1bf1455727d
martinhbramwell <martinhbramwell@gmail.com>
2012-01-12 09:21:36 -0500

    Update tools/smartgit/InstallSmartGit.sh

--- tools/smartgit/InstallSmartGit.sh
+++ tools/smartgit/InstallSmartGit.sh
@@ -1,2 +1,30 @@
-?
+#! /bin/bash
+# script to collect all my cloud DNS configuration into this directory.
+#
+export ADMIN_USERZ_UID=yourself
+export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
+export ADMIN_USERZ_WORK_DIR=/home/$ADMIN_USERZ_UID/tmp
+mkdir -p $ADMIN_USERZ_WORK_DIR
+#
+# Initiate downloading the installers we're going to need.
+cd ${INS}
+LOCAL_MIRROR=http://openerpns.warehouseman.com/downloads
+# Obtain Syntevo SYNTEVO
+SRV_SYNTEVO="http://www.syntevo.com"
+#wget -cNb --output-file=dldJdk.log ${SRV_SYNTEVO}/download/smartgit/smartgit-generic-2_1_6.tar.gz
+sudo rm -f dldSmartgit.log*
+echo "Obtaining SmartGit.."
+wget -cNb --output-file=dldSmartgit.log ${LOCAL_MIRROR}/smartgit-generic-2_1_6.tar.gz
+#
+export SYNTEVO_HOME=${PRG}/com/syntevo
+sudo mkdir -p ${SYNTEVO_HOME}
+cd ${SYNTEVO_HOME}
+echo "Expanding SmartGit.."
+sudo tar zxvf ${INS}/smartgit-generic-2_1_6.tar.gz
+echo "Symlinking SmartGit.."
+sudo ln -s smartgit-2_1_6 smartgit
+export SMARTGIT_HOME=${SYNTEVO_HOME}/smartgit
+echo "Creatng panel button.."
 
+
+    
