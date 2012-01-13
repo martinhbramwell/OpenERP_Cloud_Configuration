4d9b73b1c5a9f16398a201ce9439043bd16d0648
Martin "Hasan" Bramwell <martinhbramwell@gmail.com>
2012-01-13 17:49:17 -0500

    Update tools/LXDE/Install_LXDE_Apps.sh

--- tools/LXDE/Install_LXDE_Apps.sh
+++ tools/LXDE/Install_LXDE_Apps.sh
@@ -4,17 +4,22 @@
 export ADMIN_USERZ_UID=yourself
 export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
 #
+sudo aptitude -y install gedit
+#
 # Initiate downloading the installers we're going to need.
-cd ${INS}
 LOCAL_MIRROR=http://openerpns.warehouseman.com/downloads
+#
+cd ${INS}
 #
 sudo rm -f dldLXDE_Tools.log*
 echo "Obtaining LXDE Tools.."
 # Obtain LXDE Tools
 SRV_SOURCEFORGE="http://http://sourceforge.net/projects"
-#wget -cNb --output-file=dldJdk.log ${SRV_SOURCEFORGE}/lxde/files/latest/download?source=files
+#wget -cNb --output-file=dldLXDE_Tools.log ${SRV_SOURCEFORGE}/lxde/files/latest/download?source=files
 wget -cNb --output-file=dldLXDE_Tools.log ${LOCAL_MIRROR}/gpicview_0.1.8ubuntu1_i386.deb
 #
+${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldLXDE_Tools.log -p gpicview_0.1.8
+
 echo "Installing LXShortcut ..."
 
 
