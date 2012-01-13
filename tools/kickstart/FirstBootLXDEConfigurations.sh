acc60d2abeb81766b09fe87c7819672af75003ec
Martin "Hasan" Bramwell <martinhbramwell@gmail.com>
2012-01-13 17:33:14 -0500

    Update tools/kickstart/FirstBootLXDEConfigurations.sh

--- tools/kickstart/FirstBootLXDEConfigurations.sh
+++ tools/kickstart/FirstBootLXDEConfigurations.sh
@@ -8,6 +8,8 @@
 export INS="/home/yourself/installers"
 export PRG="/home/yourself/programs"
 export FAILURE_NOTICE="______Looks_like_it_failed______"
+3
+export SRV_CONFIG="https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master"
 #
 export ADMIN_USERZ_UID=yourself
 export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
@@ -16,4 +18,8 @@ mkdir -p $ADMIN_USERZ_WORK_DIR
 #
 echo "Get LXDE configurator"
 #
+# Obtain LXDE Configurator script
+cd ${PRG}/installTools
+wget ${SRV_CONFIG}/tools/LXDE/Install_LXDE_Apps.sh
+chmod +x ./Install_LXDE_Apps.sh
 #
