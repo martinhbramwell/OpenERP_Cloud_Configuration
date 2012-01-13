c2c6cfa72d12b0384fee35551c5f51498e82cfaa
Martin "Hasan" Bramwell <martinhbramwell@gmail.com>
2012-01-13 17:27:26 -0500

    Update tools/kickstart/FirstBootLXDEConfigurations.sh

--- tools/kickstart/FirstBootLXDEConfigurations.sh
+++ tools/kickstart/FirstBootLXDEConfigurations.sh
@@ -5,7 +5,15 @@
 #
 # sudo apt-get -y install gedit
 #
-
+export INS="/home/yourself/installers"
+export PRG="/home/yourself/programs"
+export FAILURE_NOTICE="______Looks_like_it_failed______"
+#
+export ADMIN_USERZ_UID=yourself
+export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
+export ADMIN_USERZ_WORK_DIR=/home/$ADMIN_USERZ_UID/tmp
+mkdir -p $ADMIN_USERZ_WORK_DIR
+#
 echo "Get LXDE configurator"
 #
 #
