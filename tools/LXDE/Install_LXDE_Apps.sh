4752fc1c9104c4e9f3f9a5c6d732919d5a335877
Martin "Hasan" Bramwell <martinhbramwell@gmail.com>
2012-01-14 04:51:01 -0500

    Update tools/LXDE/Install_LXDE_Apps.sh

--- tools/LXDE/Install_LXDE_Apps.sh
+++ tools/LXDE/Install_LXDE_Apps.sh
@@ -19,7 +19,8 @@ echo "Obtaining LXDE Tools.."
 # Obtain LXDE Tools
 SRV_SOURCEFORGE="http://http://sourceforge.net/projects"
 #wget -cNb --output-file=dldLXDE_Tools.log ${SRV_SOURCEFORGE}/lxde/files/latest/download?source=files
-wget -cNb --output-file=dldLXDE_Tools.log ${LOCAL_MIRROR}/gpicview_0.1.8ubuntu1_i386.deb
+wget -cNb --output-file=dldLXDE_Tools.log ${LOCAL_MIRROR}/gpicview_0.2.2-1_amd64.deb
+#
 #
 ${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldLXDE_Tools.log -p gpicview_0.1.8
 #
