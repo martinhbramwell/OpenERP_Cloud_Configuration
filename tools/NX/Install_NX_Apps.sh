61ccaaed2422ca15aec1387aad43e4bbd2b9242b
Hasan <martinhbramwell@gmail.com>
2012-01-16 15:11:11 -0500

    Control installation of NX Free.

---
+++ tools/NX/Install_NX_Apps.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# script to collect all my cloud DNS configuration into this directory.
+#
+export ADMIN_USERZ_UID=yourself
+export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
+export ADMIN_USERZ_WORK_DIR=/home/$ADMIN_USERZ_UID/tmp
+mkdir -p $ADMIN_USERZ_WORK_DIR
+#
+# Initiate downloading the installers we're going to need.
+cd ${INS}
+LOCAL_MIRROR=http://openerpns.warehouseman.com/downloads
+# Obtain NX
+SRV_NX="http://64.34.161.181/download/3.5.0/Linux"
+
+#wget -cNb --output-file=dldNXc.log ${SRV_NX}/nxclient_3.5.0-7_amd64.deb
+sudo rm -f dldNXc.log*
+echo "Obtaining FreeNX client.."
+wget -cNb --output-file=dldNXc.log ${LOCAL_MIRROR}/nxclient_3.5.0-7_amd64.deb
+#
+#wget -cNb --output-file=dldNXn.log ${SRV_NX}/nxnode_3.5.0-7_amd64.deb
+sudo rm -f dldNXn.log*
+echo "Obtaining FreeNX node.."
+wget -cNb --output-file=dldNXn.log ${LOCAL_MIRROR}/nxnode_3.5.0-7_amd64.deb
+#
+#wget -cNb --output-file=dldNXs.log ${SRV_NX}/nxserver_3.5.0-9_amd64.deb
+sudo rm -f dldNXs.log*
+echo "Obtaining FreeNX server.."
+wget -cNb --output-file=dldNXs.log ${LOCAL_MIRROR}/nxserver_3.5.0-9_amd64.deb
+#
+${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldNXc.log -p nxclient_3.5
+sudo dpkg -i nxclient_3.5.0-7_amd64.deb
+#
+${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldNXn.log -p nxnode_3.5
+sudo dpkg -i nxnode_3.5.0-7_amd64.deb
+#
+${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldNXs.log -p nxserver_3.5
+sudo dpkg -i nxserver_3.5.0-9_amd64.deb
+#
+exit 0;
+
---
+++ tools/kickstart/FirstBootNXConfigurations.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# 
+sudo apt-get -y update
+sudo apt-get -y upgrade
+#
+sudo apt-get -y install gedit
+#
+export INS="/home/yourself/installers"
+export PRG="/home/yourself/programs"
+export FAILURE_NOTICE="______Looks_like_it_failed______"
+#
+export SRV_CONFIG="https://raw.github.com/martinhbramwell/OpenERP_Cloud_Configuration/master"
+#
+export ADMIN_USERZ_UID=yourself
+export ADMIN_USERZ_HOME=/home/$ADMIN_USERZ_UID
+export ADMIN_USERZ_WORK_DIR=/home/$ADMIN_USERZ_UID/tmp
+mkdir -p $ADMIN_USERZ_WORK_DIR
+#
+echo "Get LXDE configurator"
+#
+# Obtain LXDE Configurator script
+cd ${PRG}/installTools
+rm -f ./Install_NX_Apps.sh
+wget ${SRV_CONFIG}/tools/LXDE/Install_NX_Apps.sh
+chmod +x ./Install_NX_Apps.sh
+#
+#
+echo "Running NX configurator now ..."
+#
+./Install_NX_Apps.sh
