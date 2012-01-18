2c0137ecdb483f980fed15643e83c0bc18667051
Martin "Hasan" Bramwell <martinhbramwell@gmail.com>
2012-01-18 10:04:52 -0500

    Update tools/waitForJenkins.sh

--- tools/waitForJenkins.sh
+++ tools/waitForJenkins.sh
@@ -4,20 +4,23 @@ SERVICE="Jenkins"
 command="jenkins/reload"
 response="Please wait while Jenkins is getting ready to work"
 service="jenkins/"
-expect="Please <a href=\"newJob\">create new jobs</a> to get started."
+expect1="Manage Jenkins"
+expect2="Please <a href=\"newJob\">create new jobs</a> to get started."
 
 outer=0
 inner=0
-rslt=0
+rslt1=0
+rslt2=0
 idx=0
 while [ $outer -lt 2 ]
 do
    curl -s "http://localhost/${command}" | grep "${response}"
    while [ $inner -lt 5 ]
    do
-     rslt=$(curl -s "http://localhost/${service}" | grep -c "${expect}" )
+     rslt1=$(curl -s "http://localhost/${service}" | grep -c "${expect1}" )
+     rslt2=$(curl -s "http://localhost/${service}" | grep -c "${expect2}" )
 
-     if [ ${rslt} == 1 ]
+     if [ (${rslt1} == 1) || (${rslt2} == 1) ]
      then
        echo "${SERVICE} is up and running,                                                   "
        exit 0
@@ -35,6 +38,5 @@ done
 echo "Got no answer from ${SERVICE}.                                                     "
 
 exit 1
-
 
 
