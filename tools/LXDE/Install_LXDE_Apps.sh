0c446971dc61b82da9edbf683fcf5bd256cad915
Jenkins <jenkins@warehouseman.com>
2012-01-22 19:46:34 -0500

    Make sure directory exists before trying to use it.

--- tools/LXDE/Install_LXDE_Apps.sh
+++ tools/LXDE/Install_LXDE_Apps.sh
@@ -42,8 +42,11 @@ cd /usr/share/lxde/wallpapers
 wget -cN ${LOCAL_MIRROR}/tanday.jpg
 #
 echo "Get desktop definitions..."
+mkdir -p $ADMIN_USERZ_HOME/.config/pcmanfm/LXDE
 cd $ADMIN_USERZ_HOME/.config/pcmanfm/LXDE
 wget ${SRV_CONFIG}/tools/LXDE/pcmanfm/pcmanfm.conf
+#
+mkdir -p $ADMIN_USERZ_HOME/.config/lxsession/LXDE
 cd $ADMIN_USERZ_HOME/.config/lxsession/LXDE
 wget ${SRV_CONFIG}/tools/LXDE/desktop.conf
 #
