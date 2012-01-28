215afbff59abce751b721e5b328a05db01f06627
Jenkins <jenkins@warehouseman.com>
2012-01-27 19:40:32 -0500

    Tested on its own.  Works.

--- tools/ant/installApacheAnt.sh
+++ tools/ant/installApacheAnt.sh
@@ -19,17 +19,22 @@ export DEFAULT_APPLICATIONS_DIR=/usr/share
 #
 # Prerequisite : waitForLogFileEvent.sh * * * 
 #
-rm -f ${PRG}/installTools/waitForLogFileEvent.sh
-wget ${SRV_CONFIG}/tools/waitForLogFileEvent.sh
-chmod +x ${PRG}/installTools/waitForLogFileEvent.sh
+if [ ! -f ${PRG}/installTools/waitForLogFileEvent.sh ]
+then
+	echo "Need to get waitForLogFileEvent.sh."
+	cd ${PRG}/installTools
+	rm -f ${PRG}/installTools/waitForLogFileEvent.sh
+	wget ${SRV_CONFIG}/tools/waitForLogFileEvent.sh
+	chmod +x ${PRG}/installTools/waitForLogFileEvent.sh
+fi
 #
-echo "Get Ant from local file server."
+echo "Now we can get Ant from local file server."
 cd ${INS}
 #
 SRV_APACHE_ANT="http://ftp.wayne.edu/apache/ant/"
 #wget -cNb --output-file=dldJdk.log ${SRV_APACHE_ANT}/binaries/apache-ant-1.8.2-bin.tar.gz
 sudo rm -f dldApacheAnt.log*
-echo "Obtaining SmartGit.."
+echo "Obtaining Apache Ant ..."
 wget -cNb --output-file=dldApacheAnt.log ${LOCAL_MIRROR}/apache-ant-1.8.2-bin.tar.gz
 #
 #
@@ -43,17 +48,21 @@ cd $DEFAULT_APPLICATIONS_DIR
 sudo tar -zxvf ${INS}/apache-ant-1.8.2-bin.tar.gz
 #
 # Make a permanent name for it
+sudo rm -f ant
 sudo ln -s ./apache-ant-1.8.2/ ant
 #
 # And a local environment variable
 export ANT_HOME=/usr/share/ant
 #
-echo "Check it works..."
-sudo -Hu jenkins ssh -T git@github.com
+sudo cp /etc/environment envtmp
+sudo sed -i "/JAVA_HOME=/aANT_HOME=${ANT_HOME}" envtmp
+sudo sed -i "/^PATH=/ s/\$/:\$ANT_HOME\/bin/" envtmp
+sudo mv envtmp /etc/environment
 #
-echo "Permit SmartGit access too."
-sudo -u $JENKINS_USERZ_UID chmod 660 id_rsa*
-ls -la 
+export PATH=$PATH:$ANT_HOME/bin
+echo "Check it works...$PATH "
+ant -version
+
 
 
 
