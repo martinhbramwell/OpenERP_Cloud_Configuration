30a14f67493ba33bdbe1960876b79256ecd3e605
Jenkins <jenkins@warehouseman.com>
2012-02-10 17:09:50 -0500

    Corrected sudo mismatch.

--- tools/rundeck/installRunDeck.sh
+++ tools/rundeck/installRunDeck.sh
@@ -10,7 +10,7 @@ export INS="${ADMIN_USERZ_HOME}/installers"
 export PRG="${ADMIN_USERZ_HOME}/programs"
 #
 export OUR_USERZ_UID=rundeck
-export OUR_USERZ_HOME=/home/${OUR_USERZ_UID}
+export OUR_USERZ_HOME=/var/lib/${OUR_USERZ_UID}
 echo "Preparing RunDeck for ${OUR_USERZ_UID}."
 #
 # Initiate downloading the installers we're going to need.
@@ -24,18 +24,23 @@ sudo rm -f dldRunDeck.log*
 echo "Obtaining RunDeck ..."
 wget -cNb --output-file=dldRunDeck.log ${LOCAL_MIRROR}/rundeck-1.4.1-1.deb
 #
-echo "Clear any problem packages"
+echo "Clear any problem packages.............."
 sudo aptitude -y update
 sudo aptitude -y upgrade
 sudo aptitude -fy install
 #
+echo "Create the ${OUR_USERZ_UID} user..........."
 export PASS_HASH=$(perl -e 'print crypt($ARGV[0], "password")' "okokok")
 echo ${PASS_HASH}
 sudo useradd -Ds /bin/bash
-sudo useradd -m -G admin,sudo -p ${PASS_HASH} ${OUR_USERZ_UID}
+sudo useradd -m -G admin,sudo -d ${OUR_USERZ_HOME} -p ${PASS_HASH} ${OUR_USERZ_UID}
 #
+sudo chown -R ${OUR_USERZ_UID}:${OUR_USERZ_UID} ${OUR_USERZ_HOME}
+#
+echo "Generate a key..........."
 sudo su - ${OUR_USERZ_UID} -c "${PRG}/installTools/genSSH_key.sh"
 #
+echo "Terminate the password of ${OUR_USERZ_UID} ..........."
 sudo passwd -e ${OUR_USERZ_UID}
 #
 #
--- tools/ssh/genSSH_key.sh
+++ tools/ssh/genSSH_key.sh
@@ -10,17 +10,16 @@ SSH_DIR=${CURRENT_DIR}/.ssh
 #		
 echo "Make a place for the RSA key at ${SSH_DIR} on behalf of user ${CURRENT_UID}..."
 #
-sudo rm -fr ${SSH_DIR}/
+rm -fr ${SSH_DIR}/
 #
-sudo -u ${CURRENT_UID} mkdir -p ${SSH_DIR}
-sudo chmod 770 ${SSH_DIR}
-sudo chown -R ${CURRENT_UID}:${CURRENT_UID} ${CURRENT_DIR}
+mkdir -p ${SSH_DIR}
+chmod 770 ${SSH_DIR}
 #
 echo "Make RSA key.."  ##   It'd be better to get it locally.  See below.
 #
-sudo -u ${CURRENT_UID} ssh-keygen -N "aPassword" -t rsa -f ${SSH_DIR}/id_rsa
-sudo chmod -R 660 ${SSH_DIR}/id_rsa
-sudo chmod -R 660 ${SSH_DIR}/id_rsa.pub
+ssh-keygen -N "aPassword" -t rsa -f ${SSH_DIR}/id_rsa
+chmod -R 660 ${SSH_DIR}/id_rsa
+chmod -R 660 ${SSH_DIR}/id_rsa.pub
 
 ls -l ${SSH_DIR}
 echo "= = = Done = = = "
