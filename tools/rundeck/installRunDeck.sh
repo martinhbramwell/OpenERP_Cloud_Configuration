fa0850aeedd783d48dccb2a7211f4b1c399cf481
Jenkins <jenkins@warehouseman.com>
2012-02-11 06:02:06 -0500

    RunDeck needs its scripts to be managed under version control

--- tools/rundeck/installRunDeck.sh
+++ tools/rundeck/installRunDeck.sh
@@ -3,15 +3,24 @@
 #
 export ADMIN_USERZ_UID=yourself
 export ADMIN_USERZ_HOME=/home/${ADMIN_USERZ_UID}
+export ADMIN_USERZ_DEV_DIR=/home/${ADMIN_USERZ_UID}/dev
 export ADMIN_USERZ_WORK_DIR=/home/${ADMIN_USERZ_UID}/tmp
 mkdir -p ${ADMIN_USERZ_WORK_DIR}
+#
+export GIT_MANAGED_PROJECT=RunDeckToolSet
+export GIT_MANAGED_DIR=${ADMIN_USERZ_DEV_DIR}/${GIT_MANAGED_PROJECT}
+#
+export GIT_SOURCE=git@github.com:martinhbramwell
+export MASTER_PROJECT=${GIT_SOURCE}/${GIT_MANAGED_PROJECT}.git
 #
 export INS="${ADMIN_USERZ_HOME}/installers"
 export PRG="${ADMIN_USERZ_HOME}/programs"
 #
-export OUR_USERZ_UID=rundeck
-export OUR_USERZ_HOME=/var/lib/${OUR_USERZ_UID}
-echo "Preparing RunDeck for ${OUR_USERZ_UID}."
+export RUNDECK_USER=RunDeck
+export RUNDECK_USERZ_UID=rundeck
+export RUNDECK_USERZ_HOME=/var/lib/${RUNDECK_USERZ_UID}
+export RUNDECK_USERZ_SSH_DIR=${RUNDECK_USERZ_HOME}/.ssh
+echo "Preparing the RunDeck server for user : ${RUNDECK_USERZ_UID}."
 #
 # Initiate downloading the installers we're going to need.
 cd ${INS}
@@ -29,24 +38,24 @@ sudo aptitude -y update
 sudo aptitude -y upgrade
 sudo aptitude -fy install
 #
-echo "Create the ${OUR_USERZ_UID} user..........."
+echo "Create the ${RUNDECK_USERZ_UID} user..........."
 export PASS_HASH=$(perl -e 'print crypt($ARGV[0], "password")' "okokok")
 echo ${PASS_HASH}
 sudo useradd -Ds /bin/bash
-sudo useradd -m -G admin,sudo -d ${OUR_USERZ_HOME} -p ${PASS_HASH} ${OUR_USERZ_UID}
+sudo useradd -m -G admin,sudo -d ${RUNDECK_USERZ_HOME} -p ${PASS_HASH} ${RUNDECK_USERZ_UID}
 #
-sudo chown -R ${OUR_USERZ_UID}:${OUR_USERZ_UID} ${OUR_USERZ_HOME}
+sudo chown -R ${RUNDECK_USERZ_UID}:${RUNDECK_USERZ_UID} ${RUNDECK_USERZ_HOME}
 #
-echo "Generate a key..........."
-sudo su - ${OUR_USERZ_UID} -c "${PRG}/installTools/genSSH_key.sh"
+echo "Generate a key ..........."
+sudo su - ${RUNDECK_USERZ_UID} -c "${PRG}/installTools/genSSH_key.sh"
 #
-echo "Terminate the password of ${OUR_USERZ_UID} ..........."
-sudo passwd -e ${OUR_USERZ_UID}
+echo "Terminate the password of ${RUNDECK_USERZ_UID} ..........."
+sudo passwd -e ${RUNDECK_USERZ_UID}
 #
 #
 ${PRG}/installTools/waitForCompleteDownload.sh -d 3600 -l ./dldRunDeck.log -p rundeck
 #
-cd ${OUR_USERZ_HOME}
+cd ${RUNDECK_USERZ_HOME}
 echo "export JAVA_HOME=/usr/lib/jvm/jdk" >> .bashrc 
 echo "PATH=\$PATH:\$JAVA_HOME/bin" >> .bashrc 
 echo "" >> .bashrc 
@@ -56,9 +65,33 @@ source .bash_profile
 echo "Installing RunDeck where it wants to go ..."
 sudo dpkg -i ${INS}/rundeck-1.4.1-1.deb
 #
-sudo chown -R ${OUR_USERZ_UID}:${OUR_USERZ_UID} ${OUR_USERZ_HOME}
+sudo chown -R ${RUNDECK_USERZ_UID}:${RUNDECK_USERZ_UID} ${RUNDECK_USERZ_HOME}
 #
-echo "Clear any problem packages"
+echo "Obtain our RunDeck projects .............."
+
+#
+echo "Prepare a Git Repo to contain the ${RUNDECK_USER} Project : "
+#
+mkdir -p ${ADMIN_USERZ_DEV_DIR}
+cd ${ADMIN_USERZ_DEV_DIR}
+rm -fr ${GIT_MANAGED_PROJECT} 
+#
+echo "Clone the whole Cloud project into the Git managed directory :"
+echo "The next step requires tight security so use 600 ...."
+sudo -Hu ${RUNDECK_USERZ_UID} chmod 600 ${RUNDECK_USERZ_SSH_DIR}/*
+echo sudo -Hu ${RUNDECK_USERZ_UID} git clone ${MASTER_PROJECT} ${GIT_MANAGED_PROJECT}
+sudo -Hu $RUNDECK_USERZ_UID git clone ${MASTER_PROJECT} ${GIT_MANAGED_PROJECT}
+echo "Undo tight security so ${RUNDECK_USER} & SmartGit can share the key"
+sudo -Hu ${RUNDECK_USERZ_UID} chmod 660 ${RUNDECK_USERZ_SSH_DIR}/*
+echo "SmartGit needs to own the whole hierarchy ..."
+sudo chown -R ${ADMIN_USERZ_UID}:${ADMIN_USERZ_UID} ${GIT_MANAGED_PROJECT}
+
+
+
+
+
+#
+echo "Clear any problem packages ..............."
 sudo aptitude -y update
 sudo aptitude -y upgrade
 sudo aptitude -fy install
