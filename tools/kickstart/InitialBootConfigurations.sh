d1a5526cc40ffe829fc661f78040db4ae14a59d8
Hasan <martinhbramwell@fleetingclouds.net>
2012-02-15 20:25:06 -0500

    Bad name

--- tools/kickstart/InitialBootConfigurations.sh
+++ tools/kickstart/InitialBootConfigurations.sh
@@ -71,7 +71,7 @@ start)
    ./FirstBootMySqlConfigurations.sh         2>&1 | tee "ini_${NUM: -3}_mysql.log"    && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
    ./FirstBootCyclosConfigurations.sh        2>&1 | tee "ini_${NUM: -3}_cyclos.log"   && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT}
 # Don't allow these scripts to be called ever again.
-   ./FirstBootCleanUp.sh                     2>&1 | tee "ini_${NUM: -3}_initial.log"  && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 			
+   ./FirstBootCleanUp.sh                     2>&1 | tee "ini_${NUM: -3}_final.log"  && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 			
 #
 
 ;;
