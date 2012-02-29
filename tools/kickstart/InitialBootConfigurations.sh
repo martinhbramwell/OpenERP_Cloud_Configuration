96008e6a55bc86719732c9bcc1f74f47f4ff22a1
Hasan <martinhbramwell@gmail.com>
2012-02-29 13:43:23 -0500

    Wrong name for log file.

--- tools/kickstart/InitialBootConfigurations.sh
+++ tools/kickstart/InitialBootConfigurations.sh
@@ -64,7 +64,7 @@ start)
    ./FirstBootSSHConfigurations.sh           2>&1 | tee "ini_${NUM: -3}_ssh.log"      && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
    ./FirstBootSmartGitConfigurations.sh      2>&1 | tee "ini_${NUM: -3}_smartgit.log" && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
    ./FirstBootLXDEConfigurations.sh          2>&1 | tee "ini_${NUM: -3}_lxde.log"     && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
-   ./FirstBootChefConfigurations.sh          2>&1 | tee "ini_${NUM: -3}_rundeck.log"  && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
+   ./FirstBootChefConfigurations.sh          2>&1 | tee "ini_${NUM: -3}_chef.log"     && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
 
 if [ 0 == 1 ]; then
    ./FirstBootJenkinsConfigurations.sh       2>&1 | tee "ini_${NUM: -3}_jenkins.log"  && CNT=$((  CNT += 1  ))  &&  NUM=000${CNT} 
